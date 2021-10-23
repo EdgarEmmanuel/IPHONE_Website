3d4fdcf4a3bd6c3e11285cbac5df407cc152b7a0
Edgar <mmnledgar@gmail.com>
2021-10-23 02:30:11 +0000

    make update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarPartComponent } from './navbar-part/navbar-part.component';
 import { MainpartComponent } from './mainpart/mainpart.component';
 import { FooterpartComponent } from './footerpart/footerpart.component';
+import {IphoneSeComponent} from './parts/iphone-se/iphone-se.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarPartComponent,
     MainpartComponent,
-    FooterpartComponent
+    FooterpartComponent,
+    IphoneSeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/parts/iphone-se/iphone-se.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-part-iphone-se',
+  templateUrl: './iphone-se.component.html',
+  styleUrls: ['./iphone-se.component.css']
+})
+export class IphoneSeComponent{}
